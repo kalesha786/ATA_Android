e0f02b3bb082a9af5ea3788a063e1b5fc0d1a439
Kalesha Shaik <kaleshashaik@Kaleshas-MacBook-Air.local>
2024-05-31 23:04:58 +0530

    first commit

--- public/client.js
+++ public/client.js
@@ -1,28 +1,32 @@
 const publicVapidKey = "BOd2EQ8LTe3KAgMX9lWwTlHTRzv1Iantw50Mw6pUnsNr3pcxl8iglUs-YlQEQLo4UbJk9oyXs_BxgyAe0TCqKME";
 
 if ('serviceWorker' in navigator) {
-    registerServiceWorker().catch(console.log);
-
     document.getElementById('subscribeButton').addEventListener('click', async () => {
-        const register = await registerServiceWorker();
+        try {
+            const register = await registerServiceWorker();
+            const permission = await Notification.requestPermission();
 
-        const permission = await Notification.requestPermission();
-        if (permission === 'granted') {
-            const subscription = await register.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: publicVapidKey,
-            });
+            if (permission === 'granted') {
+                const subscription = await register.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: publicVapidKey,
+                });
 
-            await fetch("/subscribe", {
-                method: "POST",
-                body: JSON.stringify(subscription),
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            });
-            console.log("permission granted..!");
-        } else {
-            throw new Error('Push permission denied');
+                await fetch("http://localhost:3000/subscribe", {
+                    method: "POST",
+                    body: JSON.stringify(subscription),
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                });
+                alert("Subscription successful!");
+            } else {
+                alert('Push permission denied');
+                throw new Error('Push permission denied');
+            }
+        } catch (error) {
+            console.error('Error during service worker registration or push subscription:', error);
+            alert(`Error: ${error.message}`);
         }
     });
 }
