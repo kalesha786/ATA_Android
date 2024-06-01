3be859ea51110ba78fc995d00614c1c7116612fa
Kalesha Shaik <kaleshashaik@Kaleshas-MacBook-Air.local>
2024-06-02 00:19:06 +0530

    latest code

--- public/client.js
+++ public/client.js
@@ -1,38 +1,87 @@
-const publicVapidKey = "BOd2EQ8LTe3KAgMX9lWwTlHTRzv1Iantw50Mw6pUnsNr3pcxl8iglUs-YlQEQLo4UbJk9oyXs_BxgyAe0TCqKME";
+async function initServiceWorker() {
+    let swRegistration = await navigator.serviceWorker.register('./worker.js', {
+        scope: '/'
+    })
+    let pushManager = swRegistration.pushManager;
 
-if ('serviceWorker' in navigator) {
-    document.getElementById('subscribeButton').addEventListener('click', async () => {
-        try {
-            const register = await registerServiceWorker();
-            const permission = await Notification.requestPermission();
+    if (!isPushManagerActive(pushManager)) {
+        return;
+    }
 
-            if (permission === 'granted') {
-                const subscription = await register.pushManager.subscribe({
-                    userVisibleOnly: true,
-                    applicationServerKey: publicVapidKey,
-                });
+    let permissionState = await pushManager.permissionState({userVisibleOnly: true});
+    switch (permissionState) {
+        case 'prompt':
+            alert("click Subscribe Push Notifications");
+            break;
+        case 'granted':
+            displaySubscriptionInfo(await pushManager.getSubscription())
+            break;
+        case 'denied':
+            alert('User denied push permission');
+    }
+}
 
-                await fetch("/subscribe", {
-                    method: "POST",
-                    body: JSON.stringify(subscription),
-                    headers: {
-                        "Content-Type": "application/json",
-                    }
-                });
-                alert("Subscription successful!");
-            } else {
-                alert('Push permission denied');
-                throw new Error('Push permission denied');
-            }
-        } catch (error) {
-            console.error('Error during service worker registration or push subscription:', error);
-            alert(`Error: ${error.message}`);
+function isPushManagerActive(pushManager) {
+    if (!pushManager) {
+        if (!window.navigator.standalone) {
+            alert("For ATA notifications, you may need to add this website to Home Screen at your iPhone or iPad.");
+        } else {
+            throw new Error('PushManager is not active');
         }
+        return false;
+    } else {
+        return true;
+    }
+}
+
+async function subscribeToPush() {
+
+    const VAPID_PUBLIC_KEY = "BOd2EQ8LTe3KAgMX9lWwTlHTRzv1Iantw50Mw6pUnsNr3pcxl8iglUs-YlQEQLo4UbJk9oyXs_BxgyAe0TCqKME";
+
+    let swRegistration = await navigator.serviceWorker.getRegistration();
+    let pushManager = swRegistration.pushManager;
+    if (!isPushManagerActive(pushManager)) {
+        return;
+    }
+    let subscriptionOptions = {
+        userVisibleOnly: true,
+        applicationServerKey: VAPID_PUBLIC_KEY
+    };
+    try {
+        let subscription = await pushManager.subscribe(subscriptionOptions);
+
+        await fetch("/subscribe", {
+            method: "POST",
+            body: JSON.stringify(subscription),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        testSend(subscription);
+        // Here you can send fetch request with subscription data to your backend API for next push sends from there
+    } catch (error) {
+        alert("User denied push permission");
+    }
+}
+
+
+function testSend() {
+    const title = "Push title";
+    const options = {
+        body: "Additional text with some description",
+        icon: "https://andreinwald.github.io/webpush-ios-example/images/push_icon.jpg",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/1920px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg",
+        data: {
+            "url": "https://andreinwald.github.io/webpush-ios-example/?page=success",
+            "message_id": "your_internal_unique_message_id_for_tracking"
+        },
+    };
+    navigator.serviceWorker.ready.then(async function (serviceWorker) {
+        await serviceWorker.showNotification(title, options);
     });
 }
 
-async function registerServiceWorker() {
-    return await navigator.serviceWorker.register('./worker.js', {
-        scope: '/'
-    });
+if (navigator.serviceWorker) {
+    initServiceWorker();
 }
--- public/worker.js
+++ public/worker.js
@@ -3,13 +3,36 @@ console.log("Service worker loaded...");
 self.addEventListener('push', event => {
     const data = event.data.json();
     console.log('Push received:', data);
-
+    if (!data || !data.title) {
+        console.error('Received WebPush with an empty title. Received body: ', data);
+    }
     const options = {
         body: data.body,
         icon: './img/icon.png'
     };
 
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
+       self.registration.showNotification(data.title, options)
+       .then(() => {
+        // You can save to your analytics fact that push was shown
+        // fetch('https://your_backend_server.com/track_show?message_id=' + pushData.data.message_id);
+    });
+});
+
+self.addEventListener('notificationclick', function (event) {   
+    event.notification.close();
+
+    if (!event.notification.data) {
+        console.error('Click on WebPush with empty data, where url should be. Notification: ', event.notification)
+        return;
+    }
+    if (!event.notification.data.url) {
+        console.error('Click on WebPush without url. Notification: ', event.notification)
+        return;
+    }
+
+    clients.openWindow(event.notification.data.url)
+        .then(() => {
+            // You can send fetch request to your analytics API fact that push was clicked
+            // fetch('https://your_backend_server.com/track_click?message_id=' + pushData.data.message_id);
+        });
 });
